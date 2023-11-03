262981d605cbd09c325c666d25e3f0b652b4a0d6
Diego Alexis Estrada Gonzalez <122661159+IDGS-803-20001735@users.noreply.github.com>
2023-11-03 13:18:29 -0600

    Boton Hover Arreglado - imagen evento central

--- components/events/EventSection.tsx
+++ components/events/EventSection.tsx
@@ -1,58 +1,57 @@
 /* eslint-disable @next/next/no-img-element */
-import { Box, Typography } from '@mui/material';
-import React from 'react';
-import { IEvent } from '../../interfaces';
-import { CountdownTimer } from './CountdownTimer';
-import styles from './EventSection.module.css';
+import { Box, Typography } from "@mui/material";
+import React from "react";
+import { IEvent } from "../../interfaces";
+import { CountdownTimer } from "./CountdownTimer";
+import styles from "./EventSection.module.css";
 
 type Props = {
-    event: IEvent
+  event: IEvent;
 };
 
 export const EventSection = ({ event }: Props) => {
-    const eventDateInMilliseconds = new Date(event.date).getTime();
+  const eventDateInMilliseconds = new Date(event.date).getTime();
 
-    return (
+  return (
+    <Box>
+      <Typography
+        variant="h2"
+        sx={{
+          fontSize: { xs: "30px", sm: "6vw" },
+          textAlign: { xs: "center", sm: "left" },
+          marginTop: "20px",
+        }}
+      >
+        {event.title}
+      </Typography>
+      <Typography
+        sx={{
+          fontSize: { md: "18px", lg: "22px" },
+          textAlign: { xs: "center", sm: "left" },
+        }}
+      >
+        {event.description}
+      </Typography>
+
+      {event.date && (
         <Box>
-            <Typography 
-                variant="h2"
-                sx={{
-                    fontSize: { xs: '30px', sm: '6vw' },
-                    textAlign: { xs: 'center', sm: 'left'},
-                    marginTop: '20px'
-                }}
-            >
-                {event.title}
-            </Typography>
-            <Typography
-                sx={{
-                    fontSize: { md: '18px', lg: '22px' },
-                    textAlign: { xs: 'center', sm: 'left' }
-                }}
-            >
-                {event.description}
-            </Typography>
-
-            {
-                event.date && (
-                    <Box>
-                        <CountdownTimer targetDate={eventDateInMilliseconds} />
-                    </Box>
-                )
-            }
-
-            <Box
-                sx={{
-                    width: '100%',
-                    height: 'auto'
-                }}
-            >
-                <img
-                    src={event.image}
-                    alt={event.title}
-                    className={styles['event-image']}
-                />
-            </Box>
+          <CountdownTimer targetDate={eventDateInMilliseconds} />
         </Box>
-    );
+      )}
+
+      <Box
+        sx={{
+          width: "100%",
+          height: "auto",
+        }}
+      >
+        <img
+          src={event.image}
+          alt={event.title}
+          className={styles["event-image"]}
+          style={{ objectFit: "cover", width: "100%", height: "100%" }}
+        />
+      </Box>
+    </Box>
+  );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,31 @@
-import { useContext, useEffect } from 'react';
-import type { NextPage } from 'next';
-import { GetServerSideProps } from 'next'
-import { useRouter } from 'next/router';
-import { Fab, Tooltip } from '@mui/material';
-import { motion, AnimatePresence } from 'framer-motion';
-import WhatsAppIcon from '@mui/icons-material/WhatsApp';
-import { ShopLayout } from '../components/layouts';
-import { Banner, Loader } from '../components/ui';
-import { ProductsSlider } from '../components/products';
-import { EventSection } from '../components/events';
-import { useProducts } from '../hooks';
-import { UIContext } from '../context';
-import { IEvent } from '../interfaces';
-import { dbEvents } from '../database';
-import { SellOutlined } from '@mui/icons-material';
+import { useContext, useEffect } from "react";
+import type { NextPage } from "next";
+import { GetServerSideProps } from "next";
+import { useRouter } from "next/router";
+import { Fab, Tooltip } from "@mui/material";
+import { motion, AnimatePresence } from "framer-motion";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import { ShopLayout } from "../components/layouts";
+import { Banner, Loader } from "../components/ui";
+import { ProductsSlider } from "../components/products";
+import { EventSection } from "../components/events";
+import { useProducts } from "../hooks";
+import { UIContext } from "../context";
+import { IEvent } from "../interfaces";
+import { dbEvents } from "../database";
+import { SellOutlined } from "@mui/icons-material";
 
 type Props = {
   events: IEvent[];
 };
 
 const defaultEvent = {
-  title: 'Eventos',
-  description: 'Encuentra los mejores eventos aquí, sigue nuestra página para que no te los pierdas.',
-  image: 'https://res.cloudinary.com/killer-diller/image/upload/v1683597421/killer-diller/loader/cr-image-2.jpg'
-}
+  title: "Eventos",
+  description:
+    "Encuentra los mejores eventos aquí, sigue nuestra página para que no te los pierdas.",
+  image:
+    "https://res.cloudinary.com/killer-diller/image/upload/v1683597421/killer-diller/loader/cr-image-2.jpg",
+};
 
 const HomePage: NextPage<Props> = ({ events }: Props) => {
   const event1 = events[0] || defaultEvent;
@@ -38,97 +40,101 @@ const HomePage: NextPage<Props> = ({ events }: Props) => {
   }, [isApplicationLoaded]);
 
   useEffect(() => {
-    if (window.location.hash = '#_=_') {
-      window.location.hash = '';
-      history.pushState('', document.title, window.location.pathname);
+    if ((window.location.hash = "#_=_")) {
+      window.location.hash = "";
+      history.pushState("", document.title, window.location.pathname);
     }
   });
-  
-  const { products } = useProducts('/products');
 
-  return (
-    !isApplicationLoaded ? (
-      <motion.div key='loader'>
-        <Loader setIsApplicationLoaded={setIsApplicationLoaded} />
-      </motion.div>
-    ) : (
-      <AnimatePresence>
-        <ShopLayout title='Killer Diller | Inicio' pageDescription='Encuentra los mejores productos aquí.'>
-          <motion.video
-            initial={{
-              opacity: 0,
-              y: 50,
-              z: 200
-            }}
-            animate={{
-              opacity: 1,
-              y: 0,
-              z: 0,
-              transition: {
-                ease: 'easeOut',
-                duration: 1,
-              }
-            }}
-            transition={{
-              ease: 'easeOut'
-            }}
-            layoutId="main-image"
-            style={{
-              width: '100%',
-              maxHeight: '700px',
-              objectFit: 'cover'
-            }}
-            autoPlay
-            loop
-            muted
-            playsInline
-            poster='https://res.cloudinary.com/killer-diller/image/upload/v1683597422/killer-diller/loader/cr-main.jpg'
-          >
-            <source 
-              src='https://res.cloudinary.com/killer-diller/video/upload/v1683597469/killer-diller/loader/banner.mp4' 
-              type='video/mp4' 
-            />
-            <source 
-              src='https://res.cloudinary.com/killer-diller/video/upload/v1683597469/killer-diller/loader/banner.mp4' 
-              type='video/ogg' 
-            />
-          </motion.video>
+  const { products } = useProducts("/products");
 
-          <Banner />
+  return !isApplicationLoaded ? (
+    <motion.div key="loader">
+      <Loader setIsApplicationLoaded={setIsApplicationLoaded} />
+    </motion.div>
+  ) : (
+    <AnimatePresence>
+      <ShopLayout
+        title="Killer Diller | Inicio"
+        pageDescription="Encuentra los mejores productos aquí."
+      >
+        <motion.video
+          initial={{
+            opacity: 0,
+            y: 50,
+            z: 200,
+          }}
+          animate={{
+            opacity: 1,
+            y: 0,
+            z: 0,
+            transition: {
+              ease: "easeOut",
+              duration: 1,
+            },
+          }}
+          transition={{
+            ease: "easeOut",
+          }}
+          layoutId="main-image"
+          style={{
+            width: "100%",
+            maxHeight: "700px",
+            objectFit: "cover",
+          }}
+          autoPlay
+          loop
+          muted
+          playsInline
+          poster="https://res.cloudinary.com/killer-diller/image/upload/v1683597422/killer-diller/loader/cr-main.jpg"
+        >
+          <source
+            src="https://res.cloudinary.com/killer-diller/video/upload/v1683597469/killer-diller/loader/banner.mp4"
+            type="video/mp4"
+          />
+          <source
+            src="https://res.cloudinary.com/killer-diller/video/upload/v1683597469/killer-diller/loader/banner.mp4"
+            type="video/ogg"
+          />
+        </motion.video>
 
-          <ProductsSlider rows={2} products={products} />
+        <Banner />
 
-          <EventSection event={event1} />
+        <ProductsSlider rows={2} products={products} />
 
-          <ProductsSlider rows={3} products={products} />
+        <EventSection event={event1} />
 
-          <EventSection event={event2} />
+        <ProductsSlider rows={3} products={products} />
 
-          <ProductsSlider rows={1} products={products} />
+        <EventSection event={event2} />
 
-          <SellWithUsFab />
-          <WhatsAppFab />
-        </ShopLayout>
-      </AnimatePresence>
-    )
+        <ProductsSlider rows={1} products={products} />
+
+        <SellWithUsFab />
+        <WhatsAppFab />
+      </ShopLayout>
+    </AnimatePresence>
   );
 };
 
 const WhatsAppFab = () => {
-  const number = '+524774409975';
-  const message = 'Hola, quiero conocer más acerca de los productos de Killer Diller.';
+  const number = "+524774409975";
+  const message =
+    "Hola, quiero conocer más acerca de los productos de Killer Diller.";
 
   return (
-    <Fab 
-      color='primary'
+    <Fab
+      color="primary"
       sx={{
-        position: 'fixed',
+        position: "fixed",
         bottom: 20,
         right: 20,
-        zIndex: 1000
+        zIndex: 1000,
       }}
-      href={`https://api.whatsapp.com/send?phone=${number}&text=${encodeURI(message)}&app_absent=0`}
-      target='blank'
+      href={`https://api.whatsapp.com/send?phone=${number}&text=${encodeURI(
+        message
+      )}&app_absent=0`}
+      target="blank"
     >
       <WhatsAppIcon />
     </Fab>
@@ -139,34 +145,31 @@ const SellWithUsFab = () => {
   const router = useRouter();
 
   return (
-    <Tooltip
-      title='Vende con nosotros'
-    >
-      <Fab 
-        color='primary'
+    <Tooltip title="Vende con nosotros">
+      <Fab
+        color="primary"
         sx={{
-          position: 'fixed',
+          position: "fixed",
           bottom: 20,
           left: 20,
-          zIndex: 1000
+          zIndex: 1000,
         }}
-        onClick={() => router.push('/sell')}
+        onClick={() => router.push("/sell")}
       >
         <SellOutlined />
       </Fab>
     </Tooltip>
   );
-}
+};
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const events = await dbEvents.getEvents();
 
   return {
     props: {
-      events: JSON.parse(JSON.stringify(events))
-    }
-  }
+      events: JSON.parse(JSON.stringify(events)),
+    },
+  };
 };
-
 
 export default HomePage;
